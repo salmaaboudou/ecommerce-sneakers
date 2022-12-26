bad3564d8113f3362961e7dd963ffa1a9f81ebf5
Salma Aboudou <salmaaboudou@MacBook-Pro-de-Salma.local>
2022-12-26 18:14:01 +0100

    Updates

---
+++ precache-manifest.b118ec2e3aa8019a9af62ffea58036a6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c04be1a3254fd3622b98",
+    "url": "/ecommerce-sneakers/static/css/main.17670ad5.chunk.css"
+  },
+  {
+    "revision": "c04be1a3254fd3622b98",
+    "url": "/ecommerce-sneakers/static/js/main.c04be1a3.chunk.js"
+  },
+  {
+    "revision": "9c43dc20a976c6f5c545",
+    "url": "/ecommerce-sneakers/static/js/1.9c43dc20.chunk.js"
+  },
+  {
+    "revision": "709cd2838a689d7f848b",
+    "url": "/ecommerce-sneakers/static/js/runtime~main.709cd283.js"
+  },
+  {
+    "revision": "b0321cb3e6bf6aef8ecc31e32a577687",
+    "url": "/ecommerce-sneakers/static/media/icon-delete.b0321cb3.svg"
+  },
+  {
+    "revision": "ca38742f874a88d7098a5344b1bf83ff",
+    "url": "/ecommerce-sneakers/static/media/icon-cart.ca38742f.svg"
+  },
+  {
+    "revision": "06305c96f6e5bbef5501440e19da4d62",
+    "url": "/ecommerce-sneakers/static/media/logo.06305c96.svg"
+  },
+  {
+    "revision": "d13f859f0bc3a337e36dd0c85251c30c",
+    "url": "/ecommerce-sneakers/static/media/icon-plus.d13f859f.svg"
+  },
+  {
+    "revision": "0035f16e8254ffaef52d8039f515581a",
+    "url": "/ecommerce-sneakers/static/media/icon-minus.0035f16e.svg"
+  },
+  {
+    "revision": "0ac65e8df3cdb3240e82aac50d5b84c5",
+    "url": "/ecommerce-sneakers/static/media/image-product-1.0ac65e8d.jpg"
+  },
+  {
+    "revision": "dc5f56d918a4a172b00d9fb5c782f873",
+    "url": "/ecommerce-sneakers/static/media/image-product-2.dc5f56d9.jpg"
+  },
+  {
+    "revision": "d8f0fd258d7833d69417c1cc12d217bb",
+    "url": "/ecommerce-sneakers/static/media/image-product-3.d8f0fd25.jpg"
+  },
+  {
+    "revision": "5e95fcb499ff1a1c449b25f41fc5bf1b",
+    "url": "/ecommerce-sneakers/static/media/image-product-4.5e95fcb4.jpg"
+  },
+  {
+    "revision": "88503524ba87127eac2976b586108ee2",
+    "url": "/ecommerce-sneakers/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ecommerce-sneakers/precache-manifest.b118ec2e3aa8019a9af62ffea58036a6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ecommerce-sneakers/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
